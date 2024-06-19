a1d8bed774439196799607c434c8c40a1229c9d5
abdilaziz Mohamed <abdilaziz.m.elsayed@gmail.com>
2024-06-19 10:30:12 +0300

    lifting gameBoard state to App comp.

--- tictactoe/src/components /GameBoard.jsx
+++ tictactoe/src/components /GameBoard.jsx
@@ -1,20 +1,8 @@
 
 import { useState } from 'react';
 
-let initialGameBoard = [
-  {id: 1, value: null, order: null, isClicked: false}, 
-  {id: 2, value: null, order: null, isClicked: false}, 
-  {id: 3, value: null, order: null, isClicked: false},
-  {id: 4, value: null, order: null, isClicked: false}, 
-  {id: 5, value: null, order: null, isClicked: false}, 
-  {id: 6, value: null, order: null, isClicked: false},
-  {id: 7, value: null, order: null, isClicked: false}, 
-  {id: 8, value: null, order: null, isClicked: false}, 
-  {id: 9, value: null, order: null, isClicked: false}
-];
 
-function GameBoard() {
-  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+function GameBoard({gameBoard, setGameBoard}) {
   const [clickCount, setClickCount] = useState(0); 
 
   const handleClick = (boxId) => {
